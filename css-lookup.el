f9d228df1898606eece81a8aa45fd7cf64633eb3
Tom Tromey <tom@tromey.com>
2017-01-16 21:13:43 -0700

    handle pseudos as well for lookup

--- css-lookup.el
+++ css-lookup.el
@@ -22,35 +22,61 @@
     (beginning-of-line)))
 
 (defconst css--mdn-symbol-regexp
-  "Regular expression to match the CSS symbol at point."
-  (concat "\\(?:\\(@" (regexp-opt css-at-ids)
-	  "\\)\\|\\(\\b" (regexp-opt css-property-ids)
-	  "\\)\\s-*\\(?:\\=\\|:\\)\\)"))
+  (concat "\\("
+	  ;; @-ids.
+	  "\\(@" (regexp-opt css-at-ids) "\\)"
+	  "\\|"
+	  ;; ;; Known properties.
+	  (regexp-opt css-property-ids t)
+	  "\\|"
+	  ;; Pseudo-classes.
+	  "\\(:" (regexp-opt css-pseudo-class-ids) "\\)"
+	  "\\|"
+	  ;; Pseudo-elements with either one or two ":"s.
+	  "\\(::?" (regexp-opt css-pseudo-element-ids) "\\)"
+	  "\\)")
+  "Regular expression to match the CSS symbol at point.")
+
+(defconst css--mdn-property-regexp
+  (concat "\\b" (regexp-opt css-property-ids t) "\\s-*\\(?:\\=\\|:\\)")
+  "Regular expression to match a CSS property.")
 
 (defconst css--mdn-completion-list
-  "List of all symbols available for lookup via MDN."
-  (nconc (mapcar (lambda (atrule) (concat "@" atrule)) css-at-ids)
-	 css-property-ids))
+  (nconc
+   ;; @-ids.
+   (mapcar (lambda (atrule) (concat "@" atrule)) css-at-ids)
+   ;; Pseudo-classes.
+   (mapcar (lambda (class) (concat ":" class)) css-pseudo-class-ids)
+   ;; Pseudo-elements with either one or two ":"s.
+   (mapcar (lambda (elt) (concat ":" elt)) css-pseudo-element-ids)
+   (mapcar (lambda (elt) (concat "::" elt)) css-pseudo-element-ids)
+   ;; Properties.
+   css-property-ids)
+  "List of all symbols available for lookup via MDN.")
 
 (defun css--find-symbol ()
+  "A helper for `css-lookup-symbol' that finds the symbol at point.
+Returns the symbol, a string, or `nil' if none found."
   (save-excursion
+    ;; Skip backward over a word first.
     (skip-chars-backward "-[:alnum:]")
+    ;; Now skip ":" or "@" to see if it's a pseudo-element or at-id.
+    (skip-chars-backward "@:")
     (if (looking-at css--mdn-symbol-regexp)
-	(or (match-string-no-properties 1)
-	    (match-string-no-properties 2))
+	(match-string-no-properties 0)
       (let ((bound (save-excursion
 		     (beginning-of-line)
 		     (point))))
-	(when (re-search-backward css--mdn-symbol-regexp bound t)
-	  (or (match-string-no-properties 1)
-	      (match-string-no-properties 2)))))))
+	(when (re-search-backward css--mdn-property-regexp bound t)
+	  (match-string-no-properties 1))))))
 
 ;;;###autoload
 (defun css-lookup-symbol (symbol)
   "Display the CSS documentation for SYMBOL, as found on MDN.
 When this command is used interactively, it picks a default
-symbol based on the CSS text before point -- either an @-keyword
-or a property name, depending on what is seen."
+symbol based on the CSS text before point -- either an @-keyword,
+a property name, a pseudo-class, or a pseudo-element, depending
+on what is seen near point."
   (interactive
    (list
     (let* ((sym (css--find-symbol))
@@ -63,6 +89,11 @@ or a property name, depending on what is seen."
 		   'css--lookup-history sym)))
       (if (equal value "") sym value))))
   (when symbol
+    ;; If we see a single-colon pseudo-element like ":after", turn it
+    ;; into "::after".
+    (when (and (eq (aref symbol 0) ?:)
+	       (member (substring symbol 1) css-pseudo-element-ids))
+      (setq symbol (concat ":" symbol)))
     (let ((url (concat css--mdn-url symbol css--mdn-xhr-params)))
       (with-current-buffer (get-buffer-create "*MDN CSS*")
 	(eww-mode)
